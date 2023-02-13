bd04bc15e80e0bd6e8b9de52f363c47847e86d28
nguyenngan0496 <memmem0496@gmail.com>
2023-02-13 20:24:58 +0700

    Buổi 11

--- 20230213-Buổi 11/script.js
+++ 20230213-Buổi 11/script.js
@@ -1,13 +1,14 @@
-alert ("Text linking script");
 
-let btnAction = document.getElementById("btn");
-btnAction.addEventListener ("click", () => {
-    let chieuDai = document.getElementById("cd");
-    let chieuRong = document.getElementById("cr");
-    if (parseFloat(chieuDai.value) == parseFloat(chieuRong.value)) {
-        console.log("Đây là hình vuông.");
+
+let btnAction1 = document.getElementById("btn-action-1");
+btnAction1.addEventListener ("click", () => {
+    let chieuDai = parseInt(document.getElementById("cd").value);
+    let chieuRong = parseInt(document.getElementById("cr").value);
+    
+    if (chieuDai == chieuRong) {
+        alert("Đây là hình vuông.");
     } else {
-        console.log("Đây không phải hình vuông.");
+        alert("Đây không phải hình vuông.");
     };
 });
 
