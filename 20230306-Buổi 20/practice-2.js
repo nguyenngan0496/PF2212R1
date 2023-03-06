2a4345015ab6068b1ad0fac69a8515595e6b83ee
nguyenngan0496 <memmem0496@gmail.com>
2023-03-06 20:49:09 +0700

    practice-2

--- 20230306-Buổi 20/practice-2.js
+++ 20230306-Buổi 20/practice-2.js
@@ -1,3 +1,6 @@
 function isPrimeNumber(number) {
-    for (let i = 2, i < number;)
+    for (let i = 2; i < number; i ++) {
+        return (number % 1 == 0 && number % i != 0)
+    }
 }
+console.log(isPrimeNumber(18))
